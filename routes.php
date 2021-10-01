b7f3ebc365e461dfa6818867fd931d59a432a1bf
Rain <rain.oselin@tptlive.ee>
2021-10-01 16:51:07 +0300

    commit

--- routes.php
+++ routes.php
@@ -2,6 +2,10 @@
 
 use App\Router;
 use App\Controller\HomeController;
+use App\Controller\PostController;
 
-Router::get('/', [HomeController::class, 'index']
-Router::get('about', HomeController::class, 'about'(){
+
+Router::get('/', [HomeController::class, 'index']);
+Router::get('/about', [HomeController::class, 'about']);
+Router::get('/posts', [PostController::class, 'posts']);
+
