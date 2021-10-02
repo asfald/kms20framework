5defa69b8e3ad3a81543e00ca684eabd425ed95b
Rain <rain.oselin@tptlive.ee>
2021-10-02 11:00:54 +0300

    commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,8 @@ namespace App\Controller;
 
 class HomeController {
     public function index(){
-        echo "Home Page";
+        $name = "Rain";
+        require __DIR__ . '/../../views/index.php';
     }
     
     public function about(){
---
+++ views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dok</title>
+</head>
+<body>
+    <h1>Home Page!</h1>
+    <p style="color: blue;" >Tere minu nimi on <?=$name;?></p>
+</body>
+</html>
